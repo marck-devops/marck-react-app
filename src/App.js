0d36f842906302496d0a4974f6da3d0e3b97d41f
marck-devops <marck.devops@gmail.com>
2025-06-21 14:10:12 -0400

    json yml pipeline

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import React from "react";
 function App() {
   return (
     <div style={{ textAlign: "center", marginTop: "2rem" }}>
-      <h1>Hello from Dockerized React. This is ready for use. By: Marck A.!</h1>
+      <h1>Hello from Dockerized React. This is ready for use. By: Marck Altidor.!</h1>
     </div>
   );
 }
